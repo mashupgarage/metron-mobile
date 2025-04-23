43cf91d98adaee19268a27d729b7752eab86ba68
vincent-mashupgarage <vincent@mashupgarage.com>
2025-04-23 18:59:41 +0800

    Update global slice to include counts for the cart items

--- src/store/slices/GlobalSlice.tsx
+++ src/store/slices/GlobalSlice.tsx
@@ -12,14 +12,16 @@ export type GlobalSlice = {
   isDarkMode: boolean;
   isLoading: boolean;
   cartCount: number;
-  cartItems: ProductT[] | [];
+  cartItems: ProductT[];
   setLoading: (value: boolean) => void;
   setDarkMode: (value: boolean) => void;
   setCartCount: (value: number) => void;
-  setCartItems: (value: []) => void;
+  setCartItems: (value: ProductT[]) => void;
+  decreaseItemQuantity: (productId: number) => void;
+  increaseItemQuantity: (productId: number) => void;
 };
 
-export const createGlobalSlice: StateCreator<GlobalSlice> = (set) => ({
+export const createGlobalSlice: StateCreator<GlobalSlice> = (set, get) => ({
   isDarkMode: false,
   isLoading: false,
   cartCount: 0,
@@ -27,5 +29,41 @@ export const createGlobalSlice: StateCreator<GlobalSlice> = (set) => ({
   setLoading: (value: boolean) => set(() => ({ isLoading: value })),
   setDarkMode: (value: boolean) => set(() => ({ isDarkMode: value })),
   setCartCount: (value: number) => set(() => ({ cartCount: value })),
-  setCartItems: (value: ProductT[]) => set(() => ({ cartItems: value })),
+  setCartItems: (value: ProductT[]) =>
+    set(() => ({
+      cartItems: value,
+      cartCount: value.length,
+    })),
+  decreaseItemQuantity: (productId: number) => {
+    const currentItems = get().cartItems;
+    const itemIndex = currentItems.findIndex((item) => item.id === productId);
+
+    if (itemIndex > -1) {
+      const newItems = [
+        ...currentItems.slice(0, itemIndex),
+        ...currentItems.slice(itemIndex + 1),
+      ];
+      set({ cartItems: newItems, cartCount: newItems.length });
+    } else {
+      console.warn(`Product with ID ${productId} not found in cart for removal.`);
+    }
+  },
+  increaseItemQuantity: (productId: number) => {
+    const currentItems = get().cartItems;
+    const productToAdd = currentItems.find((item) => item.id === productId);
+
+    if (!productToAdd) {
+      console.warn(`Product with ID ${productId} not found in cart for increasing quantity.`);
+      return;
+    }
+
+    const currentQuantityInCart = currentItems.filter(item => item.id === productId).length;
+
+    if ((productToAdd.quantity ?? 0) > currentQuantityInCart) {
+      const newItems = [...currentItems, productToAdd];
+      set({ cartItems: newItems, cartCount: newItems.length });
+    } else {
+      console.warn(`Cannot add more of Product ID ${productId}. Max quantity reached.`);
+    }
+  },
 });
