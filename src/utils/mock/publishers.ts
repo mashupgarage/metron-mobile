1d2f6ad566d512e6e0db87738946d759295bff9a
vincent-mashupgarage <vincent@mashupgarage.com>
2025-04-29 17:04:52 +0800

    Create mockdata for the comics

---
+++ src/utils/mock/publishers.ts
@@ -0,0 +1,46 @@
+export const mockPublishers = [
+  {
+    id: '1',
+    name: 'MARVEL COMICS'
+  },
+  {
+    id: '2',
+    name: 'DC COMICS'
+  },
+  {
+    id: '3',
+    name: 'IMAGE COMICS'
+  },
+  {
+    id: '4',
+    name: 'IDW PUBLISHING'
+  },
+  {
+    id: '5',
+    name: 'VALIANT ENTERTAINMENT LLC'
+  },
+  {
+    id: '6',
+    name: 'DARK HORSE COMICS'
+  },
+  {
+    id: '7',
+    name: 'ARCHIE COMIC PUBLICATIONS'
+  },
+  {
+    id: '8',
+    name: 'D. E.'
+  },
+  {
+    id: '9',
+    name: 'VIZ MEDIA LLC'
+  },
+  {
+    id: '10',
+    name: 'BOOM! STUDIOS'
+  },
+  {
+    id: '11',
+    name: 'BROWSE ALL'
+  }
+]; 
